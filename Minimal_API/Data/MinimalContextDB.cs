27ba4cbbba69133839943be7fb4d9bed51749f01
Caio-andrade24 <caio3d.andrade@gmail.com>
2023-02-02 01:33:23 -0300

    Add project files.

---
+++ Minimal_API/Data/MinimalContextDB.cs
@@ -0,0 +1,33 @@
+﻿using Minimal_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Minimal_API.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> options) : base(options) { }
+
+        public DbSet<Fornecedor> Fornecedores { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Fornecedor>()
+                .ToTable("Fornecedor");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Minimal_API/Models/Fornecedor.cs
@@ -0,0 +1,10 @@
+﻿namespace Minimal_API.Models
+{
+    public class Fornecedor
+    {
+        public Guid Id { get; set; }
+        public String? Nome { get; set; }
+        public string? Documento { get; set;}
+        public bool Ativo { get; set;}  
+    }
+}
